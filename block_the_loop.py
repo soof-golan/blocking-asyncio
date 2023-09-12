904eaac970e020c321797ee7ede9856ed3265eab
Soof Golan <soofgolan@gmail.com>
2023-09-12 20:38:43 +0300

    cleanup

--- block_the_loop.py
+++ block_the_loop.py
@@ -1,9 +1,14 @@
 import asyncio
-import time
-from typing import Awaitable, Literal
 
 from helpers import elapsed
 from log import logger
+
+
+async def some_coroutine(task_id: int):
+    """Just a placeholder for a real coroutine"""
+    logger.info("[task:%d][elapsed:%.3f] starting coroutine", task_id, elapsed())
+    await asyncio.sleep(1)
+    logger.info("[task:%d][elapsed:%.3f] ending coroutine", task_id, elapsed())
 
 
 def blocking_io(task_id: int):
@@ -14,13 +19,6 @@ def blocking_io(task_id: int):
     logger.info("[task:%d][elapsed:%.3f] ending blocking_io", task_id, elapsed())
 
 
-async def some_coroutine(task_id: int):
-    """Just a placeholder for a real coroutine"""
-    logger.info("[task:%d][elapsed:%.3f] starting coroutine", task_id, elapsed())
-    await asyncio.sleep(1)
-    logger.info("[task:%d][elapsed:%.3f] ending coroutine", task_id, elapsed())
-
-
 async def accidental_mixed_sync_and_async(task_id: int):
     """Demonstrate the accidental synchronous code"""
     logger.info("[task:%d][elapsed:%.3f] starting accidental_sync_async", task_id, elapsed())
@@ -29,27 +27,12 @@ async def accidental_mixed_sync_and_async(task_id: int):
     logger.info("[task:%d][elapsed:%.3f] ending accidental_sync_async", task_id, elapsed())
 
 
-def make_tasks(n: int, mode: Literal["async"] | Literal["accidental"]) -> list[Awaitable[None]]:
-    """Create n tasks"""
-    logger.info("creating %d tasks", n)
-    if mode == "async":
-        return [some_coroutine(task_id=i) for i in range(n)]
-    elif mode == "accidental":
-        return [accidental_mixed_sync_and_async(task_id=i) for i in range(n)]
-    else:
-        msg = f"unknown mode {mode}"
-        raise ValueError(msg)
-
-
-async def main():
-    logger.info("scheduling tasks")
-    # tasks = make_tasks(5, mode="async")
-    tasks = make_tasks(5, mode="accidental")
-
+async def main(num_tasks: int = 5):
+    logger.info("creating %d tasks", num_tasks)
+    tasks = [accidental_mixed_sync_and_async(task_id=i) for i in range(num_tasks)]
     logger.info("waiting for tasks to complete")
     await asyncio.gather(*tasks)
-
-    logger.info("exiting main")
+    logger.info("all tasks completed. exiting")
 
 
 if __name__ == "__main__":
